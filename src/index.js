5a07690690f0b02bbdf837aa9dcc457ba0a72a76
Marco Tedone <marco.tedone@gmail.com>
2020-06-06 12:23:24 +0100

    Added websocket connection from client to server

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
+const http = require('http');
+const socketio = require('socket.io');
 const app = require('./app');
+
+const server = http.createServer(app);
+const io = socketio(server);
 
 const port = process.env.PORT;
 
-app.listen(port, () => {
+io.on('connection', () => {
+    console.log('New Websocket connection');    
+});
+
+server.listen(port, () => {
     console.log(`Server is up and running at port ${port}`);    
 });
