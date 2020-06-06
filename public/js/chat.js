7a6f71f9158ad4054ad222253714e254788af611
Marco Tedone <marco.tedone@gmail.com>
2020-06-06 12:48:45 +0100

    Created the basis for a chat app

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,8 @@
-io();
+const socket = io();
+const event = socket.on('countUpdated', (count) => {
+    console.log(`Count updated to ${count}`);    
+});
+
+document.getElementById('increment').addEventListener('click', (e) => {    
+    socket.emit('updatedIncrement');    
+});
--- src/index.js
+++ src/index.js
@@ -7,9 +7,21 @@ const io = socketio(server);
 
 const port = process.env.PORT;
 
-io.on('connection', () => {
+// The chat payload
+let count = 0;
+
+io.on('connection', (socket) => {
     console.log('New Websocket connection');    
+    socket.emit('countUpdated', count);
+
+    socket.on('updatedIncrement', () => {
+        count += 1;        
+        // Emits to all socket connections as opposed to socket.emit
+        io.emit('countUpdated', count); 
+    });
 });
+
+
 
 server.listen(port, () => {
     console.log(`Server is up and running at port ${port}`);    
